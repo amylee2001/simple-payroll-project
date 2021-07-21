a9f7ebd7cdacacac7bcecf575f98d4fb47f9c369
Amy English <amy.english.developer@gmail.com>
2021-07-21 22:22:53 +0100

    Base C# Console Application

---
+++ simple-payroll-project/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace simple_payroll_project
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
